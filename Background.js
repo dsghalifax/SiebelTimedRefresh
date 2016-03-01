d2848248829dc0b1cae859a789a7a4e6624d0901
dsghalifax <nick.dollimount@software.dell.com>
2016-03-01 17:55:03 -0400

    NEW Background.js containing logic to pass ALT+Enter to Siebel tab.

---
+++ Background.js
@@ -0,0 +1,11 @@
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+    if(message.pressEnter){
+        chrome.tabs.query({active: true}, function(tabs) {
+            chrome.debugger.attach({ tabId: tabs[0].id }, "1.0");
+            chrome.debugger.sendCommand({ tabId: tabs[0].id }, 'Input.dispatchKeyEvent', { type: 'keyDown', modifiers:1, windowsVirtualKeyCode:13, nativeVirtualKeyCode : 13, macCharCode: 13  });
+            chrome.debugger.sendCommand({ tabId: tabs[0].id }, 'Input.dispatchKeyEvent', { type: 'keyUp', modifiers:1, windowsVirtualKeyCode:13, nativeVirtualKeyCode : 13, macCharCode: 13  });
+            chrome.debugger.detach({ tabId: tabs[0].id });
+            console.log('Triggered!');
+        });
+    }
+});
